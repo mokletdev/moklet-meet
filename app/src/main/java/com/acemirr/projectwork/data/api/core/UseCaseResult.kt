0a2224438864e653b0ad47825c3d0f3a43d50916
Ahmad Amirudin <ahmadamirudin901@gmail.com>
2022-04-03 19:16:16 +0700

    Refactor And update version

--- app/src/main/java/com/acemirr/projectwork/data/api/core/UseCaseResult.kt
+++
@@ -1,7 +0,0 @@
-package com.acemirr.projectwork.data.api.core
-
-sealed class UseCaseResult<out T : Any> {
-    class Success<out T : Any>(val data: T) : UseCaseResult<T>()
-    class Error(val exception: Throwable) : UseCaseResult<Nothing>()
-    class Failed(val errorMessage: String) : UseCaseResult<Nothing>()
-}
